ce5a58efb82e978163659e2f94b78e29a1c4b562
jeneewillis <87691701+jeneewillis@users.noreply.github.com>
2021-09-17 18:04:15 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', function () {
+    gsap.to("#letter-snake-1", { duration: 2.5, delay: 2, morphSVG: "#snake-1" });
+    gsap.to("#letter-snake-3", { duration: 2.5, delay: 2, morphSVG: "#snake-3" });
+    gsap.to("#letter-snake-2", { duration: 2.5, delay: 2, morphSVG: "#snake-2" });
+    gsap.to("#letter-snake-4", { duration: 2.5, delay: 2, morphSVG: "#snake-4" });
+    gsap.to("#letter-snake-5", { duration: 2.5, delay: 2, morphSVG: "#snake-5" });
+    gsap.to("#letter-snake-1", { duration: 1, delay: 2.5 });
+    gsap.to("#letter-snake-3", { duration: 1, delay: 2.5 });
+    gsap.to("#letter-snake-2", { duration: 1, delay: 2.5, "fill": "#9ac95c" });
+    gsap.to("#letter-snake-4", { duration: 1, delay: 2.5, "fill": "#dbdadb" });
+    gsap.to("#letter-snake-5", { duration: 1, delay: 2.5, "fill": "#d57d98" });
+
+    gsap.to("#letter-snake-1", { duration: 2.5, delay: 7, morphSVG: "#letter-snake-1" });
+    gsap.to("#letter-snake-3", { duration: 2.5, delay: 7, morphSVG: "#letter-snake-3" });
+    gsap.to("#letter-snake-2", { duration: 2.5, delay: 7, morphSVG: "#letter-snake-2" });
+    gsap.to("#letter-snake-4", { duration: 2.5, delay: 7, morphSVG: "#letter-snake-4" });
+    gsap.to("#letter-snake-5", { duration: 2.5, delay: 7, morphSVG: "#letter-snake-5" });
+    gsap.to("#letter-snake-1", { duration: 1, delay: 7.5 });
+    gsap.to("#letter-snake-3", { duration: 1, delay: 7.5 });
+    gsap.to("#letter-snake-2", { duration: 1, delay: 7.5, "fill": "#000" });
+    gsap.to("#letter-snake-4", { duration: 1, delay: 7.5, "fill": "#000" });
+    gsap.to("#letter-snake-5", { duration: 1, delay: 7.5, "fill": "#000" });
+
+    gsap.to("#letter-pineapple-1", { duration: 2.5, delay: 2, morphSVG: "#pineapple-1" });
+    gsap.to("#letter-pineapple-2", { duration: 2.5, delay: 2, morphSVG: "#pineapple-2" });
+    gsap.to("#letter-pineapple-3", { duration: 2.5, delay: 2, morphSVG: "#pineapple-3" });
+    gsap.to("#letter-pineapple-4", { duration: 2.5, delay: 2, morphSVG: "#pineapple-4" });
+    gsap.to("#letter-pineapple-1", { duration: 1, delay: 2.5, 'fill': "#f5ca73" });
+    gsap.to("#letter-pineapple-2", { duration: 1, delay: 2.5, 'fill': "#65aa45" });
+    gsap.to("#letter-pineapple-3", { duration: 1, delay: 2.5, 'fill': "#3e7922" });
+    gsap.to("#letter-pineapple-4", { duration: 1, delay: 2.5, 'fill': '#fdc325', 'stroke': "#886624", "stroke-width": "3px", "stroke-linejoin": "round" });
+
+    gsap.to("#letter-pineapple-1", { duration: 2.5, delay: 7, morphSVG: "#letter-pineapple-1" });
+    gsap.to("#letter-pineapple-2", { duration: 2.5, delay: 7, morphSVG: "#letter-pineapple-2" });
+    gsap.to("#letter-pineapple-3", { duration: 2.5, delay: 7, morphSVG: "#letter-pineapple-3" });
+    gsap.to("#letter-pineapple-4", { duration: 2.5, delay: 7, morphSVG: "#letter-pineapple-4" });
+    gsap.to("#letter-pineapple-1", { duration: 1, delay: 7.5, 'fill': "#000" });
+    gsap.to("#letter-pineapple-2", { duration: 1, delay: 7.5, 'fill': "#000" });
+    gsap.to("#letter-pineapple-3", { duration: 1, delay: 7.5, 'fill': "#000" });
+    gsap.to("#letter-pineapple-4", { duration: 1, delay: 7.5, 'fill': '#fff', 'stroke': "#fff" });
+
+
+})
